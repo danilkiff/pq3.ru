bf4564a64f658cbbaafa83f8b04d3a32083c2c7a
Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>
2025-06-11 15:16:46 +0300

    Use created date instead of modified

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "pq3.ru",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
